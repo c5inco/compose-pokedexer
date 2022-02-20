05187eecd4a704ba34b1a70c3de3c52e0fa5cf3c
Chris Sinco <csinco.design@gmail.com>
2022-02-19 19:31:08 -0800

    Add Accompanist for edge-to-edge

--- app/src/main/java/des/c5inco/pokedexer/MainActivity.kt
+++ app/src/main/java/des/c5inco/pokedexer/MainActivity.kt
@@ -3,17 +3,37 @@ package des.c5inco.pokedexer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import com.google.accompanist.insets.ProvideWindowInsets
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import des.c5inco.pokedexer.ui.theme.Theme.Companion.PokedexerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Turn off the decor fitting system windows, which means we need to through handling
+        // insets
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
+            // Update the system bars to be translucent
+            val systemUiController = rememberSystemUiController()
+            val useDarkIcons = MaterialTheme.colors.isLight
+            SideEffect {
+                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
+            }
+
             PokedexerTheme {
-                PokedexerApp()
+                ProvideWindowInsets {
+                    PokedexerApp()
+                }
             }
         }
     }
