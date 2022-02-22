e5a15e22e113ea2fc64e90ff0adbc71c798dc387
Chris Sinco <csinco.design@gmail.com>
2022-02-21 23:18:25 -0800

    Refactoring

---
+++ app/src/main/java/des/c5inco/pokedexer/data/pokemon/LocalPokemonRepository.kt
@@ -0,0 +1,39 @@
+package des.c5inco.pokedexer.data.pokemon
+
+import des.c5inco.pokedexer.data.Result
+import des.c5inco.pokedexer.model.Pokemon
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
+class LocalPokemonRepository(
+    private val localPokemonDataSource: LocalPokemonDataSource = LocalPokemonDataSource()
+) : PokemonRepository {
+    override suspend fun getAllPokemon(): Result<List<Pokemon>> {
+        return withContext(Dispatchers.IO) {
+            delay(800) // pretend we're on a slow network
+            if (shouldRandomlyFail()) {
+                Result.Error(IllegalStateException())
+            } else {
+                Result.Success(localPokemonDataSource.fetchPokemon())
+            }
+        }
+    }
+
+    // used to drive "random" failure in a predictable pattern, making the first request always
+    // succeed
+    private var requestCount = 0
+
+    /**
+     * Randomly fail some loads to simulate a real network.
+     *
+     * This will fail deterministically every 5 requests
+     */
+    private fun shouldRandomlyFail(): Boolean = ++requestCount % 5 == 0
+}
+
+class LocalPokemonDataSource {
+    fun fetchPokemon() : List<Pokemon> {
+        return SamplePokemonData
+    }
+}
