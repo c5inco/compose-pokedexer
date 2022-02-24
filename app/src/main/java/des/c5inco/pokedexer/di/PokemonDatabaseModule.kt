96b02ea0d521ff4023c50b1f2038b25384844eed
Chris Sinco <csinco.design@gmail.com>
2022-02-23 22:48:13 -0800

    A little more DI cleanup

--- app/src/main/java/des/c5inco/pokedexer/di/PokemonDatabaseModule.kt
+++ app/src/main/java/des/c5inco/pokedexer/di/PokemonDatabaseModule.kt
@@ -2,7 +2,6 @@ package des.c5inco.pokedexer.di
 
 import android.content.Context
 import androidx.room.Room
-import com.apollographql.apollo3.ApolloClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -10,8 +9,6 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import des.c5inco.pokedexer.data.pokemon.PokemonDao
 import des.c5inco.pokedexer.data.pokemon.PokemonDatabase
-import des.c5inco.pokedexer.data.pokemon.PokemonRepository
-import des.c5inco.pokedexer.data.pokemon.RemotePokemonRepository
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -31,21 +28,5 @@ object PokemonDatabaseModule {
             PokemonDatabase::class.java,
             "pokemon.db"
         ).build()
-    }
-
-    @Provides
-    @Singleton
-    fun provideApolloClient() : ApolloClient {
-        return ApolloClient.Builder()
-            .serverUrl("https://beta.pokeapi.co/graphql/v1beta")
-            .build()
-    }
-
-    @Provides
-    fun providePokemonRepository(
-        pokemonDao: PokemonDao,
-        apolloClient: ApolloClient
-    ): PokemonRepository {
-        return RemotePokemonRepository(pokemonDao, apolloClient)
     }
 }
