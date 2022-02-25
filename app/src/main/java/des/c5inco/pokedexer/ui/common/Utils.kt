d962c5e402fabf43ebb7ff07217fe16bac67321f
Chris Sinco <csinco.design@gmail.com>
2022-02-24 23:30:15 -0800

    Add debugger Modifier

--- app/src/main/java/des/c5inco/pokedexer/ui/common/Utils.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/common/Utils.kt
@@ -1,3 +1,12 @@
 package des.c5inco.pokedexer.ui.common
 
+import androidx.compose.foundation.border
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
 fun formatId(id: Int): String = "#" + "$id".padStart(3, '0')
+
+fun Modifier.debugBounds(width: Dp = 1.dp) =
+    border(width, Color.Magenta)
