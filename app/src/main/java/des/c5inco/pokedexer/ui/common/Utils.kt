1d2ed570985e778f7638050c68a1a370f162ae07
Chris Sinco <csinco.design@gmail.com>
2022-02-26 17:28:52 -0800

    Add toRadian util

--- app/src/main/java/des/c5inco/pokedexer/ui/common/Utils.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/common/Utils.kt
@@ -8,5 +8,7 @@ import androidx.compose.ui.unit.dp
 
 fun formatId(id: Int): String = "#" + "$id".padStart(3, '0')
 
+fun Double.toRadian(): Double = this / 180 * Math.PI
+
 fun Modifier.debugBounds(width: Dp = 1.dp) =
     border(width, Color.Magenta)
