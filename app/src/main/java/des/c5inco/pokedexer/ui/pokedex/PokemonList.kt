6684d99a9e20d1cd61bab7ad4fdd0a00a109e4fc
Chris Sinco <csinco.design@gmail.com>
2022-12-14 23:38:48 -0800

    Add navbar padding on pokedex list

--- app/src/main/java/des/c5inco/pokedexer/ui/pokedex/PokemonList.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/pokedex/PokemonList.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
@@ -182,7 +183,11 @@ fun PokemonListScreen(
     viewModel: PokedexViewModel,
     onPokemonSelected: (Pokemon) -> Unit = {}
 ) {
-    Surface(Modifier.fillMaxSize()) {
+    Surface(
+        Modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+    ) {
         Box {
             PokeBallBackground(
                 Modifier
