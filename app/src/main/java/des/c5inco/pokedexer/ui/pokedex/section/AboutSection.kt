91079d3cccf1298f3f02775117c5002f7746cdad
Chris Sinco <csinco.design@gmail.com>
2022-12-15 02:00:15 -0800

    Hook up gender data to UI

--- app/src/main/java/des/c5inco/pokedexer/ui/pokedex/section/AboutSection.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/pokedex/section/AboutSection.kt
@@ -1,53 +1,140 @@
 package des.c5inco.pokedexer.ui.pokedex.section
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import des.c5inco.pokedexer.R
 import des.c5inco.pokedexer.data.pokemon.SamplePokemonData
 import des.c5inco.pokedexer.model.Pokemon
+import des.c5inco.pokedexer.ui.common.Label
 import des.c5inco.pokedexer.ui.theme.Theme
 
 @Composable
 fun AboutSection(
     pokemon: Pokemon
 ) {
-    Column {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(28.dp)
+    ) {
         Text(
             text = pokemon.description,
             lineHeight = 24.sp
         )
-        Spacer(Modifier.height(28.dp))
         Surface(
             shape = RoundedCornerShape(12.dp),
             elevation = 12.dp
         ) {
-            Row(Modifier
-                .fillMaxWidth()
-                .padding(vertical = 16.dp, horizontal = 20.dp)
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp, horizontal = 20.dp)
             ) {
                 Column(Modifier.weight(1f)) {
-                    Text("Height", modifier = Modifier.alpha(0.4f))
+                    Label(text = "Height")
                     Spacer(Modifier.height(12.dp))
                     Text("${pokemon.height}cm")
                 }
                 Column(Modifier.weight(1f)) {
-                    Text("Weight", modifier = Modifier.alpha(0.4f))
+                    Label(text = "Weight")
                     Spacer(Modifier.height(12.dp))
                     Text("${pokemon.weight}kg")
                 }
             }
+        }
+        BreedingDetails(pokemon = pokemon)
+    }
+}
+
+@Composable
+private fun BreedingDetails(
+    modifier: Modifier = Modifier,
+    pokemon: Pokemon
+) {
+    Column(modifier) {
+        Text(
+            "Breeding",
+            style = MaterialTheme.typography.h6,
+        )
+        Spacer(Modifier.height(24.dp))
+        Row(Modifier.fillMaxWidth()) {
+            Label(
+                text = "Gender",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 12.dp)
+            )
+            if (pokemon.genderRate != -1) {
+                Row(Modifier.weight(2.5f)) {
+                    val femaleGenderRate = pokemon.genderRate * 12.5
+                    Row {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_outline_male_20),
+                            contentDescription = null,
+                            tint = Color(0xff6C79DB)
+                        )
+                        Spacer(Modifier.width(2.dp))
+                        Text("${100 - femaleGenderRate}%")
+                    }
+                    Spacer(Modifier.width(12.dp))
+                    Row {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_outline_female_20),
+                            contentDescription = null,
+                            tint = Color(0xffF0729F)
+                        )
+                        Spacer(Modifier.width(2.dp))
+                        Text("$femaleGenderRate%")
+                    }
+                }
+            } else {
+                Text(
+                    "Genderless",
+                    modifier = Modifier.weight(2.5f)
+                )
+            }
+        }
+        Spacer(Modifier.height(18.dp))
+        Row(Modifier.fillMaxWidth()) {
+            Label(
+                text = "Egg Groups",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 12.dp)
+            )
+            Text(
+                "-",
+                modifier = Modifier.weight(2.5f)
+            )
+        }
+        Spacer(Modifier.height(18.dp))
+        Row(Modifier.fillMaxWidth()) {
+            Label(
+                text = "Egg Cycles",
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(end = 12.dp)
+            )
+            Text(
+                "-",
+                modifier = Modifier.weight(2.5f)
+            )
         }
     }
 }
