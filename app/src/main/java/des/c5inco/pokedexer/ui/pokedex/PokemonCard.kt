7c75061002ce994b90c73610943c9b3fd3f81d05
Chris Sinco <csinco.design@gmail.com>
2022-12-17 12:21:35 -0800

    Refactor PokemonCard into its own file

---
+++ app/src/main/java/des/c5inco/pokedexer/ui/pokedex/PokemonCard.kt
@@ -0,0 +1,148 @@
+package des.c5inco.pokedexer.ui.pokedex
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.graphics.drawable.toBitmap
+import com.skydoves.landscapist.coil.CoilImage
+import des.c5inco.pokedexer.data.pokemon.SamplePokemonData
+import des.c5inco.pokedexer.model.Pokemon
+import des.c5inco.pokedexer.model.color
+import des.c5inco.pokedexer.ui.common.PokeBall
+import des.c5inco.pokedexer.ui.common.PokemonTypeLabels
+import des.c5inco.pokedexer.ui.common.TypeLabelMetrics
+import des.c5inco.pokedexer.ui.common.artworkUrl
+import des.c5inco.pokedexer.ui.common.formatId
+import des.c5inco.pokedexer.ui.common.placeholderPokemonImage
+import des.c5inco.pokedexer.ui.theme.Theme
+
+@Composable
+fun PokeDexCard(
+    modifier: Modifier = Modifier,
+    pokemon: Pokemon,
+    onPokemonSelected: (Pokemon) -> Unit = {}
+) {
+    Surface(
+        modifier = modifier,
+        color = pokemon.color(),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        PokeDexCardContent(
+            modifier = Modifier.clickable {
+                onPokemonSelected(pokemon)
+            },
+            pokemon = pokemon
+        )
+    }
+}
+
+@Composable
+private fun PokeDexCardContent(
+    modifier: Modifier = Modifier,
+    pokemon: Pokemon
+) {
+    Box(modifier.height(120.dp)) {
+        Column(
+            Modifier.padding(top = 24.dp, start = 12.dp)
+        ) {
+            PokemonName(pokemon.name)
+            Spacer(Modifier.height(8.dp))
+            PokemonTypeLabels(pokemon.typeOfPokemon, TypeLabelMetrics.SMALL)
+        }
+        Text(
+            text = formatId(pokemon.id),
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(top = 10.dp, end = 12.dp)
+                .graphicsLayer {
+                    alpha = 0.1f
+                }
+        )
+        PokeBall(
+            Modifier
+                .align(Alignment.BottomEnd)
+                .offset(x = 5.dp, y = 10.dp)
+                .requiredSize(96.dp),
+            Color.White,
+            0.25f
+        )
+
+        CoilImage(
+            imageModel = { artworkUrl(pokemon.image) },
+            previewPlaceholder = placeholderPokemonImage(pokemon.image),
+            success = { imageState ->
+                imageState.drawable?.let {
+                    Image(
+                        bitmap = it.toBitmap().asImageBitmap(),
+                        contentDescription = pokemon.name,
+                    )
+                }
+            },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom = 6.dp, end = 6.dp)
+                .size(80.dp)
+        )
+    }
+}
+
+@Composable
+private fun PokemonName(name: String?) {
+    Text(
+        text = name ?: "",
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        color = Color.White,
+    )
+}
+
+@Preview
+@Composable
+private fun PokeDexCardPreview() {
+    Theme.PokedexerTheme {
+        Surface {
+            Column(
+                Modifier.width(200.dp),
+                verticalArrangement = Arrangement.spacedBy(5.dp)
+            ) {
+                PokeDexCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    pokemon = SamplePokemonData[0]
+                )
+                PokeDexCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    pokemon = SamplePokemonData[3]
+                )
+                PokeDexCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    pokemon = SamplePokemonData[6]
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/des/c5inco/pokedexer/ui/pokedex/PokemonList.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/pokedex/PokemonList.kt
@@ -1,26 +1,18 @@
 package des.c5inco.pokedexer.ui.pokedex
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -29,25 +21,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.core.graphics.drawable.toBitmap
-import com.skydoves.landscapist.coil.CoilImage
 import des.c5inco.pokedexer.data.pokemon.LocalPokemonRepository
-import des.c5inco.pokedexer.data.pokemon.SamplePokemonData
 import des.c5inco.pokedexer.model.Pokemon
-import des.c5inco.pokedexer.model.color
-import des.c5inco.pokedexer.ui.common.PokeBall
 import des.c5inco.pokedexer.ui.common.PokeBallBackground
-import des.c5inco.pokedexer.ui.common.PokemonTypeLabels
-import des.c5inco.pokedexer.ui.common.TypeLabelMetrics.Companion.SMALL
-import des.c5inco.pokedexer.ui.common.artworkUrl
-import des.c5inco.pokedexer.ui.common.formatId
-import des.c5inco.pokedexer.ui.common.placeholderPokemonImage
 import des.c5inco.pokedexer.ui.theme.Theme.Companion.PokedexerTheme
 
 @Composable
@@ -97,88 +75,6 @@ fun PokemonList(
 }
 
 @Composable
-fun PokeDexCard(
-    modifier: Modifier = Modifier,
-    pokemon: Pokemon,
-    onPokemonSelected: (Pokemon) -> Unit = {}
-) {
-    Surface(
-        modifier = modifier,
-        color = pokemon.color(),
-        shape = RoundedCornerShape(16.dp)
-    ) {
-        PokeDexCardContent(
-            modifier = Modifier.clickable {
-                onPokemonSelected(pokemon)
-            },
-            pokemon = pokemon
-        )
-    }
-}
-
-@Composable
-fun PokeDexCardContent(
-    modifier: Modifier = Modifier,
-    pokemon: Pokemon
-) {
-    Box(modifier.height(120.dp)) {
-        Column(
-            Modifier.padding(top = 24.dp, start = 12.dp)
-        ) {
-            PokemonName(pokemon.name)
-            Spacer(Modifier.height(8.dp))
-            PokemonTypeLabels(pokemon.typeOfPokemon, SMALL)
-        }
-        Text(
-            text = formatId(pokemon.id),
-            fontWeight = FontWeight.Bold,
-            fontSize = 14.sp,
-            modifier = Modifier
-                .align(Alignment.TopEnd)
-                .padding(top = 10.dp, end = 12.dp)
-                .graphicsLayer {
-                    alpha = 0.1f
-                }
-        )
-        PokeBall(
-            Modifier
-                .align(Alignment.BottomEnd)
-                .offset(x = 5.dp, y = 10.dp)
-                .requiredSize(96.dp),
-            Color.White,
-            0.25f
-        )
-
-        CoilImage(
-            imageModel = { artworkUrl(pokemon.image) },
-            previewPlaceholder = placeholderPokemonImage(pokemon.image),
-            success = { imageState ->
-                imageState.drawable?.let {
-                    Image(
-                        bitmap = it.toBitmap().asImageBitmap(),
-                        contentDescription = pokemon.name,
-                    )
-                }
-            },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(bottom = 6.dp, end = 6.dp)
-                .size(80.dp)
-        )
-    }
-}
-
-@Composable
-fun PokemonName(name: String?) {
-    Text(
-        text = name ?: "",
-        fontWeight = FontWeight.Bold,
-        fontSize = 14.sp,
-        color = Color.White,
-    )
-}
-
-@Composable
 fun PokemonListScreen(
     viewModel: PokedexViewModel,
     onPokemonSelected: (Pokemon) -> Unit = {}
@@ -200,32 +96,6 @@ fun PokemonListScreen(
                 pokemon = viewModel.uiState.pokemon
             ) {
                 onPokemonSelected(it)
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun PokeDexCardPreview() {
-    PokedexerTheme {
-        Surface {
-            Column(
-                Modifier.width(200.dp),
-                verticalArrangement = Arrangement.spacedBy(5.dp)
-            ) {
-                PokeDexCard(
-                    modifier = Modifier.fillMaxWidth(),
-                    pokemon = SamplePokemonData[0]
-                )
-                PokeDexCard(
-                    modifier = Modifier.fillMaxWidth(),
-                    pokemon = SamplePokemonData[3]
-                )
-                PokeDexCard(
-                    modifier = Modifier.fillMaxWidth(),
-                    pokemon = SamplePokemonData[6]
-                )
             }
         }
     }
