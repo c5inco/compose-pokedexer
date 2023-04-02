6e9df2cfbfad206e734ecd42606cb6b91dcfb34e
Chris Sinco <csinco.design@gmail.com>
2023-04-01 21:59:01 -0700

    Add ItemImage common component

---
+++ app/src/main/java/des/c5inco/pokedexer/ui/common/ItemImage.kt
@@ -0,0 +1,40 @@
+package des.c5inco.pokedexer.ui.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.core.graphics.drawable.toBitmap
+import com.skydoves.landscapist.coil.CoilImage
+import des.c5inco.pokedexer.R
+import des.c5inco.pokedexer.model.Item
+
+@Composable
+fun ItemImage(
+    modifier: Modifier = Modifier,
+    item: Item
+) {
+    CoilImage(
+        imageModel = { itemAssetsUri(item.sprite) },
+        previewPlaceholder = R.drawable.item_flame_orb,
+        success = { imageState ->
+            imageState.drawable?.let {
+                Image(
+                    bitmap = it.toBitmap().asImageBitmap(),
+                    contentScale = ContentScale.Inside,
+                    contentDescription = null,
+                    modifier = Modifier.matchParentSize()
+                )
+            }
+        },
+        failure = {
+            PokeBall(
+                tint = MaterialTheme.colorScheme.onSurfaceVariant,
+                alpha = Emphasis.Disabled.alpha
+            )
+        },
+        modifier = modifier
+    )
+}
--- app/src/main/java/des/c5inco/pokedexer/ui/items/ItemsScreen.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/items/ItemsScreen.kt
@@ -1,6 +1,5 @@
 package des.c5inco.pokedexer.ui.items
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -23,20 +22,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.graphics.drawable.toBitmap
-import com.skydoves.landscapist.coil.CoilImage
-import des.c5inco.pokedexer.R
 import des.c5inco.pokedexer.data.items.SampleItems
 import des.c5inco.pokedexer.model.Item
-import des.c5inco.pokedexer.ui.common.Emphasis
+import des.c5inco.pokedexer.ui.common.ItemImage
 import des.c5inco.pokedexer.ui.common.NavigationTopAppBar
-import des.c5inco.pokedexer.ui.common.PokeBall
-import des.c5inco.pokedexer.ui.common.itemAssetsUri
 import des.c5inco.pokedexer.ui.theme.AppTheme
 
 @Composable
@@ -111,25 +103,8 @@ private fun ItemCard(
             Row(
                 verticalAlignment = Alignment.CenterVertically
             ) {
-                CoilImage(
-                    imageModel = { itemAssetsUri(item.sprite) },
-                    previewPlaceholder = R.drawable.item_flame_orb,
-                    success = { imageState ->
-                        imageState.drawable?.let {
-                            Image(
-                                bitmap = it.toBitmap().asImageBitmap(),
-                                contentScale = ContentScale.Inside,
-                                contentDescription = null,
-                                modifier = Modifier.matchParentSize()
-                            )
-                        }
-                    },
-                    failure = {
-                        PokeBall(
-                            tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                            alpha = Emphasis.Disabled.alpha
-                        )
-                    },
+                ItemImage(
+                    item = item,
                     modifier = Modifier.size(48.dp)
                 )
                 Spacer(Modifier.width(12.dp))
