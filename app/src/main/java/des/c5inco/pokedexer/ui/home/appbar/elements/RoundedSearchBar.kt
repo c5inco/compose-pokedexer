8787de26979845e971b6fc916343d157ef9988af
Chris Sinco <csinco.design@gmail.com>
2024-02-13 23:45:05 -0800

    Fix text overflow issue with search bar on HomeScreen

--- app/src/main/java/des/c5inco/pokedexer/ui/home/appbar/elements/RoundedSearchBar.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/home/appbar/elements/RoundedSearchBar.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -45,7 +46,8 @@ fun RoundedSearchBar() {
                 modifier = Modifier
                     .weight(1f)
                     .padding(horizontal = 12.dp),
-                color = MaterialTheme.colorScheme.onSurfaceVariant
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                overflow = TextOverflow.Ellipsis
             )
         }
     }
