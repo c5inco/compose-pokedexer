438ac0beb6970497081c1d7a05e32f7ccf500ec6
Chris Sinco <csinco.design@gmail.com>
2024-04-28 16:02:01 -0700

    Remove M2 surface import

--- app/src/main/java/des/c5inco/pokedexer/ui/common/PokeBall.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/common/PokeBall.kt
@@ -2,7 +2,7 @@ package des.c5inco.pokedexer.ui.common
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Surface
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
--- app/src/main/java/des/c5inco/pokedexer/ui/common/PokeBallBackground.kt
+++ app/src/main/java/des/c5inco/pokedexer/ui/common/PokeBallBackground.kt
@@ -3,7 +3,7 @@ package des.c5inco.pokedexer.ui.common
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Surface
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
