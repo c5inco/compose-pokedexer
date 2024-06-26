0df0cdbc2f14cb27294a85ef270e1da4e924bbc9
Chris Sinco <csinco.design@gmail.com>
2024-06-25 20:48:15 -0700

    Initial commit

--- app/src/main/kotlin/des/c5inco/pokedexer/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/des/c5inco/pokedexer/ui/home/HomeScreen.kt
@@ -1,11 +1,21 @@
-@file:OptIn(ExperimentalFoundationApi::class)
-
 package des.c5inco.pokedexer.ui.home
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,11 +26,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import des.c5inco.pokedexer.model.Type
 import des.c5inco.pokedexer.ui.home.appbar.MainAppBar
 import des.c5inco.pokedexer.ui.home.appbar.SearchResult
+import des.c5inco.pokedexer.ui.home.appbar.search.ItemResultCardExpanded
 import des.c5inco.pokedexer.ui.theme.AppTheme
 
 sealed class MenuItem(
@@ -35,33 +49,88 @@ sealed class MenuItem(
     object TypeCharts : MenuItem("Type charts", Type.Psychic)
 }
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun HomeScreen(
     onMenuItemSelected: (MenuItem) -> Unit = { _ -> },
     onSearchResultSelected: (SearchResult) -> Unit = { _ -> }
 ) {
     var openAlertDialog by remember { mutableStateOf(false) }
+    var expandSearchResult by remember { mutableStateOf(false) }
+    var searchResult by remember { mutableStateOf<SearchResult?>(null) }
 
-    Surface(
-        Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column {
-            MainAppBar(
-                onMenuItemSelected = {
-                    when (it) {
-                        MenuItem.Moves,
-                        MenuItem.Pokedex,
-                        MenuItem.Items ->
-                            onMenuItemSelected(it)
-                        else ->
-                            openAlertDialog = true
-                    }
-                },
-                onSearchResultSelected = onSearchResultSelected
-            )
+    SharedTransitionLayout {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Box {
+                Column {
+                    MainAppBar(
+                        selectedSearchResult = searchResult,
+                        sharedTransitionScope = this@SharedTransitionLayout,
+                        onMenuItemSelected = {
+                            when (it) {
+                                MenuItem.Moves,
+                                MenuItem.Pokedex,
+                                MenuItem.Items ->
+                                    onMenuItemSelected(it)
+
+                                else ->
+                                    openAlertDialog = true
+                            }
+                        },
+                        onSearchResultSelected = {
+                            expandSearchResult = true
+                            searchResult = it
+                            //onSearchResultSelected(it)
+                        }
+                    )
+                }
+            }
         }
+        AnimatedVisibility(
+            visible = expandSearchResult,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                searchResult?.let {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable {
+                                expandSearchResult = false
+                                searchResult = null
+                            }
+                            .background(Color.Black.copy(alpha = 0.5f))
+                    )
+                    when (it) {
+                        is SearchResult.PokemonEvent -> TODO()
+                        is SearchResult.ItemEvent -> {
+                            ItemResultCardExpanded(
+                                item = it.item,
+                                animatedVisibilityScope = this@AnimatedVisibility
+                            )
+                        }
+
+                        is SearchResult.MoveEvent -> TODO()
+                    }
+                }
+            }
+        }
+
+        // SearchResultExpanded(
+        //     result = searchResult,
+        //     onDismiss = {
+        //         expandSearchResult = false
+        //     }
+        // )
     }
+
 
     if (openAlertDialog) {
         AlertDialog(
@@ -84,6 +153,46 @@ fun HomeScreen(
                 }
             },
         )
+    }
+}
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+private fun SharedTransitionScope.SearchResultExpanded(
+    modifier: Modifier = Modifier,
+    result: SearchResult?,
+    onDismiss: () -> Unit = { }
+) {
+    AnimatedContent(
+        modifier = modifier,
+        targetState = result,
+        transitionSpec = {
+            fadeIn() togetherWith fadeOut()
+        },
+    ) { targetResult ->
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            targetResult?.let {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable { onDismiss() }
+                        .background(Color.Black.copy(alpha = 0.5f))
+                )
+                when (it) {
+                    is SearchResult.PokemonEvent -> TODO()
+                    is SearchResult.ItemEvent -> {
+                        ItemResultCardExpanded(
+                            item = it.item,
+                            animatedVisibilityScope = this@AnimatedContent
+                        )
+                    }
+                    is SearchResult.MoveEvent -> TODO()
+                }
+            }
+        }
     }
 }
 
--- app/src/main/kotlin/des/c5inco/pokedexer/ui/home/appbar/search/ItemResultCard.kt
+++ app/src/main/kotlin/des/c5inco/pokedexer/ui/home/appbar/search/ItemResultCard.kt
@@ -1,13 +1,24 @@
 package des.c5inco.pokedexer.ui.home.appbar.search
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
@@ -16,23 +27,40 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import des.c5inco.pokedexer.data.items.SampleItems
 import des.c5inco.pokedexer.model.Item
 import des.c5inco.pokedexer.ui.common.ItemImage
 import des.c5inco.pokedexer.ui.theme.AppTheme
+import kotlin.math.exp
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-fun ItemResultCard(
+fun SharedTransitionScope.ItemResultCard(
     modifier: Modifier = Modifier,
-    item: Item = SampleItems.first()
+    item: Item = SampleItems.first(),
+    animatedVisibilityScope: AnimatedVisibilityScope,
+    onSelected: (Item) -> Unit = { }
 ) {
     Card(
         modifier = modifier
-            .width(200.dp),
+            .width(200.dp)
+            .clickable { onSelected(item) }
+            .sharedBounds(
+                sharedContentState = rememberSharedContentState(key = "item-${item.id}-bounds"),
+                animatedVisibilityScope = animatedVisibilityScope,
+                //clipInOverlayDuringTransition = OverlayClip(shapeForSharedElement)
+            ),
         shape = MaterialTheme.shapes.large,
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.secondaryContainer
@@ -49,47 +77,138 @@ fun ItemResultCard(
                 text = item.name,
                 style = MaterialTheme.typography.bodyMedium,
             )
-            ItemImage(item = item)
+            ItemImage(
+                item = item,
+                modifier = Modifier.sharedElement(
+                    state = rememberSharedContentState(key = "image-${item.id}"),
+                    animatedVisibilityScope = animatedVisibilityScope
+                )
+            )
         }
     }
 }
 
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Preview
 @Composable
 fun ItemResultCardPreview() {
     AppTheme {
         Column(
             modifier = Modifier
-                .fillMaxSize()
                 .padding(vertical = 20.dp)
         ) {
-            LazyHorizontalGrid(
-                rows = GridCells.Fixed(3),
-                contentPadding = PaddingValues(horizontal = 20.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier
-                    .height(240.dp)
-                    .fillMaxWidth()
-            ) {
-                item {
-                    ItemResultCard()
-                }
-                item {
-                    ItemResultCard()
-                }
-                item {
-                    ItemResultCard()
-                }
-                item {
-                    ItemResultCard()
-                }
-                item {
-                    ItemResultCard()
+            SharedTransitionLayout {
+                AnimatedContent(
+                    targetState = true,
+                ) { _ ->
+                    LazyHorizontalGrid(
+                        rows = GridCells.Fixed(3),
+                        contentPadding = PaddingValues(horizontal = 20.dp),
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(16.dp),
+                        modifier = Modifier
+                            .height(240.dp)
+                            .fillMaxWidth()
+                    ) {
+                        items(5) {
+                            ItemResultCard(
+                                animatedVisibilityScope = this@AnimatedContent
+                            )
+                        }
+                    }
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+fun SharedTransitionScope.ItemResultCardExpanded(
+    modifier: Modifier = Modifier,
+    item: Item = SampleItems.last(),
+    animatedVisibilityScope: AnimatedVisibilityScope
+) {
+    Box {
+        Card(
+            modifier = modifier
+                .width(300.dp)
+                .sharedBounds(
+                    sharedContentState = rememberSharedContentState(key = "item-${item.id}-bounds"),
+                    animatedVisibilityScope = animatedVisibilityScope,
+                    //clipInOverlayDuringTransition = OverlayClip(shapeForSharedElement)
+                ),
+            shape = MaterialTheme.shapes.large,
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(top = 32.dp, start = 12.dp, end = 12.dp, bottom = 20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = item.name,
+                    style = MaterialTheme.typography.headlineSmall,
+                    color = MaterialTheme.colorScheme.primary,
+                    textAlign = TextAlign.Center
+                )
+                Spacer(Modifier.height(4.dp))
+                Text(
+                    text = item.description,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+        ItemImage(
+            item = item,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(48.dp)
+                .align(Alignment.TopCenter)
+                .offset(y = -24.dp)
+                .sharedElement(
+                    state = rememberSharedContentState(key = "image-${item.id}"),
+                    animatedVisibilityScope = animatedVisibilityScope
+                )
+        )
+    }
+}
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Preview
+@Composable
+private fun ItemResultCardExpandedPreview() {
+    var expanded by remember { mutableStateOf(false) }
+
+    AppTheme {
+        SharedTransitionLayout {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                AnimatedContent(
+                    targetState = expanded,
+                ) { targetState ->
+                    if (targetState) {
+                        ItemResultCardExpanded(
+                            modifier = Modifier.clickable { expanded = false },
+                            animatedVisibilityScope = this@AnimatedContent
+                        )
+                    } else {
+                        ItemResultCard(
+                            modifier = Modifier.clickable { expanded = true },
+                            animatedVisibilityScope = this@AnimatedContent
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+}
