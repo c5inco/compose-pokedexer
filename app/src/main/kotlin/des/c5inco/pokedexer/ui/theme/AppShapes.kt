1d19c7d8d4c546f6143cf27e557cbf9c0adba66a
Chris Sinco <csinco.design@gmail.com>
2024-07-02 22:06:35 -0700

    Add new super ellipse shape

--- app/src/main/kotlin/des/c5inco/pokedexer/ui/theme/AppShapes.kt
+++ app/src/main/kotlin/des/c5inco/pokedexer/ui/theme/AppShapes.kt
@@ -2,10 +2,57 @@ package des.c5inco.pokedexer.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Matrix
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.toPath
+import kotlin.math.max
 
 val AppShapes = Shapes(
     small = RoundedCornerShape(4.dp),
     medium = RoundedCornerShape(4.dp),
     large = RoundedCornerShape(16.dp)
 )
+
+fun RoundedPolygon.getBounds() = calculateBounds().let { Rect(it[0], it[1], it[2], it[3]) }
+
+class SuperEllipse(
+    private var matrix: Matrix = Matrix(),
+    private var rotation: Float = 45f
+) : Shape {
+    private var path = Path()
+    private val polygon = RoundedPolygon(
+        numVertices = 4,
+        rounding = CornerRounding(
+            radius = 0.4f,
+            smoothing = 1f
+        )
+    )
+
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        path.rewind()
+        path = polygon.toPath().asComposePath()
+        matrix.reset()
+        val bounds = polygon.getBounds()
+        val maxDimension = max(bounds.width, bounds.height)
+        matrix.scale(size.width / maxDimension, size.height / maxDimension)
+        matrix.translate(-bounds.left, -bounds.top)
+        matrix.rotateZ(rotation)
+
+        path.transform(matrix)
+        return Outline.Generic(path)
+    }
+}
