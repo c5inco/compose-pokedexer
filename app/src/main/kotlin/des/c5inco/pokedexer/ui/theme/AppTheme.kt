d35af56192a79773c43851367003c87d3460c3c5
Chris Sinco <csinco@google.com>
2024-07-18 14:54:48 -0700

    Replace generated M3 colors from Figma with library

--- app/src/main/kotlin/des/c5inco/pokedexer/ui/theme/AppTheme.kt
+++ app/src/main/kotlin/des/c5inco/pokedexer/ui/theme/AppTheme.kt
@@ -5,93 +5,25 @@ import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.contentColorFor
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
 import com.materialkolor.PaletteStyle
-import com.materialkolor.ktx.contrastRatio
 import com.materialkolor.ktx.darken
 import com.materialkolor.ktx.lighten
 import com.materialkolor.rememberDynamicColorScheme
 import des.c5inco.pokedexer.model.MoveCategory
 import des.c5inco.pokedexer.model.Type
 
-private val M3LightColors = lightColorScheme(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary,
-    primaryContainer = md_theme_light_primaryContainer,
-    onPrimaryContainer = md_theme_light_onPrimaryContainer,
-    secondary = md_theme_light_secondary,
-    onSecondary = md_theme_light_onSecondary,
-    secondaryContainer = md_theme_light_secondaryContainer,
-    onSecondaryContainer = md_theme_light_onSecondaryContainer,
-    tertiary = md_theme_light_tertiary,
-    onTertiary = md_theme_light_onTertiary,
-    tertiaryContainer = md_theme_light_tertiaryContainer,
-    onTertiaryContainer = md_theme_light_onTertiaryContainer,
-    error = md_theme_light_error,
-    errorContainer = md_theme_light_errorContainer,
-    onError = md_theme_light_onError,
-    onErrorContainer = md_theme_light_onErrorContainer,
-    background = SurfaceTones.light2,
-    onBackground = md_theme_light_onBackground,
-    surface = md_theme_light_surface,
-    onSurface = md_theme_light_onSurface,
-    surfaceVariant = md_theme_light_surfaceVariant,
-    onSurfaceVariant = md_theme_light_onSurfaceVariant,
-    outline = md_theme_light_outline,
-    inverseOnSurface = md_theme_light_inverseOnSurface,
-    inverseSurface = md_theme_light_inverseSurface,
-    inversePrimary = md_theme_light_inversePrimary,
-    surfaceTint = md_theme_light_surfaceTint,
-    outlineVariant = md_theme_light_outlineVariant,
-    scrim = md_theme_light_scrim,
-)
-
-private val M3DarkColors = darkColorScheme(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    primaryContainer = md_theme_dark_primaryContainer,
-    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-    secondary = md_theme_dark_secondary,
-    onSecondary = md_theme_dark_onSecondary,
-    secondaryContainer = md_theme_dark_secondaryContainer,
-    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-    tertiary = md_theme_dark_tertiary,
-    onTertiary = md_theme_dark_onTertiary,
-    tertiaryContainer = md_theme_dark_tertiaryContainer,
-    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-    error = md_theme_dark_error,
-    errorContainer = md_theme_dark_errorContainer,
-    onError = md_theme_dark_onError,
-    onErrorContainer = md_theme_dark_onErrorContainer,
-    background = SurfaceTones.dark,
-    onBackground = md_theme_dark_onBackground,
-    surface = md_theme_dark_surface,
-    onSurface = md_theme_dark_onSurface,
-    surfaceVariant = md_theme_dark_surfaceVariant,
-    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-    outline = md_theme_dark_outline,
-    inverseOnSurface = md_theme_dark_inverseOnSurface,
-    inverseSurface = md_theme_dark_inverseSurface,
-    inversePrimary = md_theme_dark_inversePrimary,
-    surfaceTint = md_theme_dark_surfaceTint,
-    outlineVariant = md_theme_dark_outlineVariant,
-    scrim = md_theme_dark_scrim,
-)
-
 @Composable
 fun AppTheme(
     useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (!useDarkTheme) {
-        M3LightColors
-    } else {
-        M3DarkColors
-    }
+    val colors = rememberDynamicColorScheme(
+        seedColor = Color(0xff673AB7),
+        isDark = useDarkTheme,
+    )
 
     MaterialTheme(
         colorScheme = colors,
@@ -159,15 +91,21 @@ fun mapDynamicPokemonColorScheme(
             surfaceVariant = colorScheme.onPrimary
         )
     } else {
+        // PokemonTypeColorScheme(
+        //     primary = seedColor.lighten(0.7f),
+        //     surface = seedColor,
+        //     onSurface = if (seedColor.contrastRatio(colorScheme.onSecondary) > 2.2) {
+        //         colorScheme.onSecondary
+        //     } else {
+        //         colorScheme.onSecondaryContainer
+        //     },
+        //     surfaceVariant = seedColor.lighten(0.7f)
+        // )
         PokemonTypeColorScheme(
-            primary = seedColor.lighten(0.7f),
-            surface = seedColor,
-            onSurface = if (seedColor.contrastRatio(colorScheme.onSecondary) > 2.2) {
-                colorScheme.onSecondary
-            } else {
-                colorScheme.onSecondaryContainer
-            },
-            surfaceVariant = seedColor.lighten(0.7f)
+            primary = colorScheme.primaryContainer.lighten(0.5f),
+            surface = colorScheme.primaryContainer,
+            onSurface = colorScheme.onSurface,
+            surfaceVariant = colorScheme.onPrimary
         )
     }
 }
