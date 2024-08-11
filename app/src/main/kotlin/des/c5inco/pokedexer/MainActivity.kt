9440721f0d8c6b1311ffa3cdbae815159ec25c68
Chris Sinco <csinco.design@gmail.com>
2024-08-10 20:50:33 -0700

    Remove last Accompanist dep for enableEdgeToEdge

--- app/src/main/kotlin/des/c5inco/pokedexer/MainActivity.kt
+++ app/src/main/kotlin/des/c5inco/pokedexer/MainActivity.kt
@@ -2,15 +2,14 @@ package des.c5inco.pokedexer
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.view.WindowCompat
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import des.c5inco.pokedexer.ui.theme.AppTheme
 
@@ -19,17 +18,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Turn off the decor fitting system windows, which means we need to through handling
-        // insets
-        WindowCompat.setDecorFitsSystemWindows(window, false)
+        // Turn off the decor fitting system windows, which allows us to handle insets,
+        // including IME animations, and go edge-to-edge
+        // This also sets up the initial system bar style based on the platform theme
+        enableEdgeToEdge()
 
         setContent {
             // Update the system bars to be translucent
-            val systemUiController = rememberSystemUiController()
-            val useDarkIcons = !isSystemInDarkTheme()
-
-            SideEffect {
-                systemUiController.setSystemBarsColor(Color.Transparent, darkIcons = useDarkIcons)
+            val useDarkIcons = isSystemInDarkTheme()
+            DisposableEffect(useDarkIcons) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        android.graphics.Color.TRANSPARENT,
+                        android.graphics.Color.TRANSPARENT,
+                    ) { useDarkIcons },
+                )
+                onDispose {}
             }
 
             AppTheme {
