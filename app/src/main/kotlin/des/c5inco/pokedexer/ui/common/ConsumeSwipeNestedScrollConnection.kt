455d0fe57d26277fdb432322e54ac8f6244c0dbd
Chris Sinco <csinco.design@gmail.com>
2024-11-24 15:27:38 -0800

    Update deprecations

--- app/src/main/kotlin/des/c5inco/pokedexer/ui/common/ConsumeSwipeNestedScrollConnection.kt
+++ app/src/main/kotlin/des/c5inco/pokedexer/ui/common/ConsumeSwipeNestedScrollConnection.kt
@@ -16,7 +16,7 @@ fun consumeSwipeNestedScrollConnection(
 ): NestedScrollConnection = object : NestedScrollConnection {
     override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
         val delta = available.toFloat()
-        return if (delta < 0 && source == NestedScrollSource.Drag) {
+        return if (delta < 0 && source == NestedScrollSource.UserInput) {
             state.dispatchRawDelta(delta).toOffset()
         } else {
             Offset.Zero
@@ -28,7 +28,7 @@ fun consumeSwipeNestedScrollConnection(
         available: Offset,
         source: NestedScrollSource
     ): Offset {
-        return if (source == NestedScrollSource.Drag) {
+        return if (source == NestedScrollSource.UserInput) {
             state.dispatchRawDelta(available.toFloat()).toOffset()
         } else {
             Offset.Zero
