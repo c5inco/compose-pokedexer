298701e6ceb69523a1651749d856bdc50bdc10a9
Chris Sinco <csinco.design@gmail.com>
2024-11-24 15:47:52 -0800

    Use layout instead of offset modifiers for Card Content

--- app/src/main/kotlin/des/c5inco/pokedexer/ui/pokedex/section/AboutSection.kt
+++ app/src/main/kotlin/des/c5inco/pokedexer/ui/pokedex/section/AboutSection.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -43,6 +44,7 @@ fun AboutSection(
         modifier = modifier
             .verticalScroll(rememberScrollState())
             .padding(24.dp)
+            .navigationBarsPadding()
     ) {
         Text(
             text = pokemon.description,
@@ -73,7 +75,6 @@ fun AboutSection(
             AbilitiesDetails(abilities = abilities)
         }
         BreedingDetails(pokemon = pokemon)
-        Spacer(Modifier.height(128.dp))
     }
 }
 
